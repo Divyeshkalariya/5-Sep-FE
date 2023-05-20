202d5168832f75eb14d4fd2fb6b27edf6b675434
Divyeshkalariya <113225100+Divyeshkalariya@users.noreply.github.com>
2023-05-20 10:49:23 +0530

    Add files via upload

---
+++ PRACTICAL/REACT JS/lecture-1/hellow-world/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+import "./index.css"
+
+
+ReactDOM.render(
+    <>
+        <h1>mango</h1>
+        <h3>fruit</h3>
+    </>
+    ,document.getElementById('demo'));
---
+++ PRACTICAL/REACT JS/lecture-2/example-2/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react";
+
+function App(){
+    return <h1>hello world</h1>
+}
+
+
+const root= ReactDOM.createRoot(document.getElementById("demo"))
+
+root.render(<App/>);
+
+            <h1>Hellow World</h1>
